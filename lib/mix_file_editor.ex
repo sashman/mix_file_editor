a340aa52b8c780787d54e8ce6b631d8087dd6431
sashman <sashman90@gmail.com>
2018-07-25 17:02:35 +0100

    Reformatting

--- lib/mix_file_editor.ex
+++ lib/mix_file_editor.ex
@@ -13,16 +13,15 @@ defmodule MixFileEditor do
 
   """
   def deps(path) do
-    with {:ok, {:defmodule, _, module}} <- path
-      |> File.read!
-      |> Code.string_to_quoted,
-      [_, [do: {_, _, module_contents}]] <- module,
-      deps_ast <- find_deps(module_contents),
-      deps <- parse_deps_ast(deps_ast)
-     do
+    with {:ok, {:defmodule, _, module}} <-
+           path
+           |> File.read!()
+           |> Code.string_to_quoted(),
+         [_, [do: {_, _, module_contents}]] <- module,
+         deps_ast <- find_deps(module_contents),
+         deps <- parse_deps_ast(deps_ast) do
       deps
-
-     else
+    else
       _ -> :error
     end
   end
@@ -32,14 +31,13 @@ defmodule MixFileEditor do
     |> Enum.find(&deps_function/1)
   end
 
-  defp deps_function({:defp, _, [{:deps, _, nil}|_]}), do: true
+  defp deps_function({:defp, _, [{:deps, _, nil} | _]}), do: true
   defp deps_function(_), do: false
 
   defp parse_deps_ast(deps_ast) do
-    with {_,_,defp_contents} <- deps_ast,
-        do_block <- defp_contents |> Enum.at(1),
-        [do: do_contents] <- do_block
-    do
+    with {_, _, defp_contents} <- deps_ast,
+         do_block <- defp_contents |> Enum.at(1),
+         [do: do_contents] <- do_block do
       do_contents
     end
   end
